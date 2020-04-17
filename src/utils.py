050e721194c3c323a382ef538a8f502e0107d5d0
bitzj2015 <bitzj2015@outlook.com>
2020-04-17 13:27:26 -0700

    remove gap.py, loss.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 import numpy as np
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset
 
 def weights_init(m):
@@ -61,3 +62,29 @@ class ToTensor(object):
     def __call__(self, sample):
         data, user = sample['x'], sample['u']
         return {'x':torch.from_numpy(data), 'u':torch.from_numpy(user)}
+
+class GANLoss(nn.Module):
+    """Reward-Refined NLLLoss Function for adversial training of Gnerator"""
+    def __init__(self):
+        super(GANLoss, self).__init__()
+
+    def forward(self, prob, target, reward):
+        """
+        Args:
+            prob: (N, C), torch Variable
+            target : (N, ), torch Variable
+            reward : (N, ), torch Variable
+        """
+        N = target.size(0)
+        C = prob.size(1)
+        one_hot = torch.zeros((N, C))
+        if prob.is_cuda:
+            one_hot = one_hot.cuda()
+        one_hot.scatter_(1, target.data.view((-1,1)), 1)
+        one_hot = one_hot.type(torch.ByteTensor)
+        if prob.is_cuda:
+            one_hot = one_hot.cuda()
+        loss = torch.masked_select(prob, one_hot)
+        loss = loss * reward
+        loss =  -torch.sum(loss)
+        return loss
