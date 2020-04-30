34897bdf33c28202b983de8438689f66916c4a18
Ubuntu <ubuntu@ip-172-31-0-26.us-west-2.compute.internal>
2020-04-30 07:33:16 +0000

    small revision

--- src/run_batch.sh
+++ src/run_batch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python main.py --cuda=1 --batch_size=64 --phase="train_pri" --w0=0.5 --w1=0.5 --w2=0.0 --v="00"
 python main.py --cuda=1 --batch_size=64 --phase="train_pri" --w0=0.1 --w1=0.2 --w2=0.8 --v="01"
 python main.py --cuda=1 --batch_size=64 --phase="train_pri" --w0=0.1 --w1=0.8 --w2=0.2 --v="02"
 python main.py --cuda=1 --batch_size=64 --phase="train_pri" --w0=0.4 --w1=0.2 --w2=0.8 --v="03"
